c951a452f10f53dbdd650e58c931e6d036875902
Barbara Schlottfeldt Maia <31692665+babschlott@users.noreply.github.com>
2022-05-09 14:50:07 +0200

    Update streamlit_app.py
    Filtering data frame to show only selected fruits on pick list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,5 @@
 
 import streamlit
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list= my_fruit_list.set_index('Fruit')
 streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -10,5 +7,9 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index), ['Avocado','Strawberries'])
-streamlit.dataframe(my_fruit_list)
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list= my_fruit_list.set_index('Fruit')
+fruits_selected= streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index), ['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
