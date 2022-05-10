dc51327bda9eb321c9e1fb2d1f1f609299d87225
Barbara Schlottfeldt Maia <31692665+babschlott@users.noreply.github.com>
2022-05-10 12:02:46 +0200

    Update streamlit_app.py
    add kiwi to query results

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 # New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
