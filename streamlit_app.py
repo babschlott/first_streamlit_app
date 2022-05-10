6f2fc23658dd42bc8cd9a893e9ba486449d56247
Barbara Schlottfeldt Maia <31692665+babschlott@users.noreply.github.com>
2022-05-10 14:32:24 +0200

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ except URLError as e:
       
 streamlit.write('The user entered', fruit_choice)
 #Snowflake-related functions
-def get_fruit_list():
+def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("SELECT * from fruit_load_list")
     return my_cur.fetchall()
